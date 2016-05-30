05750939e73e2004e77f923a7c37f58f17a5ccba
critian franco <critian franco>
2016-05-30 22:59:04 +0200

    30/05/2016

---
+++ app/view/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" 
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<LINK rel=stylesheet href="CSS/estilo.css" type="text/css" media="screen">
+
+<html>
+
+	<head>
+		<title> KARISMA EVENTS </title>
+	</head>
+
+	<body>
+
+		<div class="capcalera">
+	        
+	        <h1>Karisma<span>EVENTS</span></a></h1>
+	        <ul class="menu">
+
+	            <li><a class="active" href="Inicio.html">P&aacute;gina principal</a></li>
+	            <li><a href="Inicio.html">Log in</a></li>
+	            <li><a href="Inicio.html">Registrarse</a></li>
+	            
+	        </ul>
+
+	    </div>
+	   
+
+</html>
